d53f0949476693d27d5e86dae75786c06da00570
Im-Marlu <immarlux@gmail.com>
2023-07-10 20:19:00 +0200

    Update
    clean update

---
+++ src/components/Information/Languages/nl.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    upperTitle: "Waarom Ons?",
+    title: "RIFCO: Professional datacenter facility services :",
+    paragraph1: "Vandaag de dag vervullen datacenters een cruciale rol bij het optimaal laten functioneren van bedrijf kritische IT-apparatuur van duizenden bedrijven en miljoenen gebruikers. Servers staan in een geklimatiseerde, veilige en schone omgeving zodat de continuïteit en optimale werking van werkplekapparatuur, telefonie en andere complexe IT-infrastructuur is gewaarborgd.",
+    paragraph2: "RIFCO levert hoogwaardige facilitaire dienstverlening om datazalen en bedrijf kritische ruimten in datacenters optimaal schoon te houden. Daarbij worden werkmethodieken, materiaalgebruik en reinigingsfrequenties afgestemd op de situatie ter plaatse. Dit alles maar met één doel: het leveren van een optimale kwaliteit met zo weinig mogelijk verstoring van het dagelijkse datacenter bedrijfsproces. Niets voor niets luidt onze slogan:",
+    button: "Lees Meer",
+}
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,79 @@
+import Image from "next/image";
+import React, { useState } from "react";
+import Logo from "../../../public/rifco_logo.png";
+import Link from "next/link";
+import NavLinks from "./NavLinks";
+import Button from "../Button";
+import LanguageSelector from "./LanguageSelector";
+import { useRouter } from "next/router";
+
+//Icons
+import {IoMdMenu, IoMdClose} from "react-icons/io"
+
+//Language
+import nl from "./Languages/nl";
+import en from "./Languages/en";
+
+const Navbar = () => {
+    const [openNav, setOpenNav] = useState(false);
+
+    //Language Stuff : 
+    const router = useRouter();
+    const { locale } = router;
+    const t = locale === 'nl' ? nl : en;
+
+
+  return (
+    <nav className="bg-white border-gray-300 border-b-2 relative z-50">
+      <div className="flex items-center font-medium justify-around">
+        <div className="z-50 p-5 md:p-0 md:w-auto w-full flex justify-between">
+          <Image
+            width={290}
+            height={114}
+            alt="Rifco Logo"
+            src={Logo}
+            className="md:cursor-pointer h-16 w-auto"
+          />
+          <div className="text-3xl md:hidden mt-4" onClick={() => setOpenNav(!openNav)}>
+                { !openNav ? <IoMdMenu /> : <IoMdClose /> }
+          </div>
+        </div>
+        <ul className="md:flex hidden uppercase font-titleFont font-semibold itemcs-center gap-8">
+          <li>
+            <Link href="/" className="py-7 px-3 inline-block">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="py-7 px-3 inline-block">
+              Webshop
+            </Link>
+          </li>
+          <NavLinks />
+        </ul>
+        <div className="md:flex hidden">
+          <Button />
+          <LanguageSelector />
+        </div>
+
+        {/* Mobile Navigation */}
+        <ul className={`
+        md:hidden bg-gray-200 absolute w-full top-0 h-fit bottom-0 pt-24 pb-4 pl-4 duration-500 rounded-b-3xl ${openNav ? 'left-0' : 'left-[-100%]'}
+        `}>
+          <li>
+            <Link href="/" className="py-7 px-3 inline-block">
+              Home
+            </Link>
+          </li>
+          <NavLinks />
+          <div className="py-5 flex justify-end mr-8">
+            <Button />
+            <LanguageSelector />
+          </div>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
