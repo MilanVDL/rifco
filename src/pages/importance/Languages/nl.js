6c00fe6128599aa6ea8b50ef6e64061041ae4470
Im-Marlu <immarlux@gmail.com>
2023-07-10 20:20:29 +0200

    s

--- src/pages/importance/Languages/nl.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-    title: "De noodzaak van datacenter reiniging",
-    p1: "Stof en vervuiling zijn een belangrijke oorzaken van storingen. De vuilgevoelige IT-apparatuur kan door stofvorming oververhit raken, waardoor het optimaal functioneren van de apparatuur in gevaar komt.",
-    p2: "Vandaar dat preventieve, en vooral regelmatige, reiniging van (onder)vloeren en interieurdelen in datazalen, patchkasten en overige technische ruimten een cruciale bijdrage levert aan het gewenste reinigingsniveau en hygiëne in data centers.",
-    p3: "RIFCO heeft de kennis en ervaring om specialistische reiniging effectief en efficiënt uit te voeren. Ook voor preventieve maatregelen om vervuiling blijvend te minimaliseren is RIFCO graag uw partner.",
-    button: "Vragen? Neem Contact Op",
-}
