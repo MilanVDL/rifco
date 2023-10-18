712cbd2a99654893f2f2966d4e26ad1926f1cbbe
MilanVDL <milan.vanderLinden@student.hu.nl>
2023-10-18 11:16:38 +0200

    test
    test

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2717de453d045a9c"; }
+/******/ 		__webpack_require__.h = function() { return "20b6320ee6898d94"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
